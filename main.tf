9126653e15ed9471ceed5e5de7b852a740025e34
Guillermo Avendaño en linux <avendano.guillermo@gmail.com>
2023-04-03 11:23:05 -0300

    kafka y postgres ready

--- main.tf
+++ main.tf
@@ -14,3 +14,26 @@ resource "helm_release" "postgres" {
     value = "${file("mobius.sql")}"
    }
 } 
+
+resource "helm_release" "kafka" {
+  name       = "kafka"
+  chart      = "${path.module}/helm/shared-kafka"
+  namespace  = "shared"
+  create_namespace = true
+
+  set {
+    name  = "image.repository"
+    value = "bitnami/kafka"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "3.3.2-debian-11-r22"
+   }
+   
+   set {
+    name  = "serviceAccount.name"
+    value = "shared-kafka"
+   }
+
+}
