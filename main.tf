7c95e45b7a4330276621fd97d3d35679480cb322
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-04-03 08:53:35 -0300

    Nuevo

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "postgres" {
+  name       = "postgres"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+  version    = "10.4.1"
+  namespace  = "default"
+
+  set {
+    name  = "postgresqlPassword"
+    value = "postgres"
+  }
+
+  set {
+    name  = "postgresqlInitdbSql"
+    value = "${data.template_file.create_db.rendered}"
+  }
+
+data "template_file" "create_db" {
+  template = <<-EOF
+    CREATE ROLE "mobiusserver12" WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD 'M0biu$2023';
+    CREATE ROLE "mobiusview12" WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD 'M0biu$2023';
+    CREATE ROLE "eventanalytics" WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD 'M0biu$2023';
+    CREATE DATABASE "mobiusserver12" WITH OWNER = "mobiusserver12" TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+    CREATE DATABASE "mobiusview12" WITH OWNER = "mobiusview12" TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+    CREATE DATABASE "eventanalytics" WITH OWNER = "eventanalytics" TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+  EOF
+}
+
+
+}
