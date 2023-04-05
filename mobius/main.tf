9f4a164ecf887086bf636610560b1aeef14b7dd5
Guillermo Avendaño en linux <avendano.guillermo@gmail.com>
2023-04-05 12:35:08 -0300

    variables mobius OK

--- mobius/main.tf
+++ mobius/main.tf
@@ -1,99 +1,93 @@
 #-- define secret for postgres password
 resource "kubernetes_secret" "mobius12" {
   metadata {
-    name = "mobius-server-secrets"
-    namespace  = var.namespace
-    labels = {
-        app.kubernetes.io/name: mobius
-        helm.sh/chart: mobius
-        app.kubernetes.io/managed-by: Helm  
-    }
+    name      = "mobius-server-secrets"
   }
 
   data = {
-    user = "${base64encode(var.mobius.MOBIUSUSERNAME)}"
-    schema = "${base64encode(var.mobius.MOBIUSSCHEMANAME)}"
-    password = "${base64encode(var.mobius.MOBIUSSCHEMAPASSWORD)}"
-    endpoint = "${base64encode(var.mobius.RDSENDPOINT)}"
-    port = "${base64encode(var.mobius.RDSPORT)}"
-    topicUrl = "${base64encode("jdbc:postgresql://${var.mobius.RDSENDPOINT}:${var.mobius.RDSPORT}/eventanalytics")}"
-    topicUser = "${base64encode(var.mobius.OPIC_EXPORT_USER)}"
-    topicPassword = "${base64encode(var.mobius.TOPIC_EXPORT_PASSWORD)}"
+    user          = "${base64encode(var.mobius["MOBIUSUSERNAME"])}"
+    schema        = "${base64encode(var.mobius["MOBIUSSCHEMANAME"])}"
+    password      = "${base64encode(var.mobius["MOBIUSSCHEMAPASSWORD"])}"
+    endpoint      = "${base64encode(var.mobius["RDSENDPOINT"])}"
+    port          = "${base64encode(var.mobius["RDSPORT"])}"
+    topicUrl      = "${base64encode("jdbc:postgresql://${var.mobius["RDSENDPOINT"]}:${var.mobius["RDSPORT"]}/eventanalytics")}"
+    topicUser     = "${base64encode(var.mobius["TOPIC_EXPORT_USER"])}"
+    topicPassword = "${base64encode(var.mobius["TOPIC_EXPORT_PASSWORD"])}"
   }
 }
 
 resource "helm_release" "mobius12" {
-  name       = "mobius12"
-  chart      = "${path.module}/helm/mobius-12.0.0"
-  namespace  = var.namespace
+  name             = "mobius12"
+  chart            = "${path.module}/helm/mobius-12.0.0"
+  namespace        = var.namespace
   create_namespace = true
 
   set {
     name  = "image.repository"
-    value = var.mobius-kube.image_repository
+    value = var.mobius-kube["image_repository"]
   }
 
   set {
     name  = "image.tag"
-    value = var.mobius-kube.image_tag
-   }
+    value = var.mobius-kube["image_tag"]
+  }
 
-   set {
-    name = "storageClassName"
-    value =  var.mobius-kube.storageClassName
-   }
-
-   set {
-    name = "mobius.admin.user"
-    value = var.mobius.MOBIUS_ADMIN_USER
-   }
-
-   set {
-    name = "mobius.admin.group"
-    value = var.mobius.MOBIUS_ADMIN_GROUP
-   }
-
-   set {
-    name = "mobius.rds.provider"
-    value = var.mobius.RDSPROVIDER
-   }
-   set {
-    name = "mobius.rds.protocol"
-    value = var.mobius.RDSPORT
-   }
-
-   set {
-    name = "mobius.fts.host"
-    value = var.mobius.MOBIUS_FTS_HOST
-   }
-
-   set {
-    name = "mobius.persistentVolume.volumeName"
-    value = var.mobius-kube.persistentVolume_volumeName
-   }
-   set {
-    name = "mobius.persistentVolume.claimName"
-    value = var.mobius-kube.persistentVolume_claimName
-   }
   set {
-    name = "mobius.persistentVolume.size"
-    value = var.mobius-kube.persistentVolume_size
-   }
+    name  = "storageClassName"
+    value = var.mobius-kube["storageClassName"]
+  }
 
-   set {
-    name = "mobius.mobiusDiagnostics.persistentVolume.volumeName"
-    value = var.mobius-kube.mobiusDiagnostics_persistentVolume_volumeName
-   }
-   set {
-    name = "mobius.mobiusDiagnostics.persistentVolume.claimName"
-    value = var.mobius-kube.mobiusDiagnostics_persistentVolume_claimName
-   }
   set {
-    name = "mobius.mobiusDiagnostics.persistentVolume.size"
-    value = var.mobius-kube.mobiusDiagnostics_persistentVolume_size
-   }
-   set {
-    name = "mobius.clustering.kubernetes.namespace"
+    name  = "mobius.admin.user"
+    value = var.mobius["MOBIUS_ADMIN_USER"]
+  }
+
+  set {
+    name  = "mobius.admin.group"
+    value = var.mobius["MOBIUS_ADMIN_GROUP"]
+  }
+
+  set {
+    name  = "mobius.rds.provider"
+    value = var.mobius["RDSPROVIDER"]
+  }
+  set {
+    name  = "mobius.rds.protocol"
+    value = var.mobius["RDSPORT"]
+  }
+
+  set {
+    name  = "mobius.fts.host"
+    value = var.mobius["MOBIUS_FTS_HOST"]
+  }
+
+  set {
+    name  = "mobius.persistentVolume.volumeName"
+    value = var.mobius-kube["persistentVolume_volumeName"]
+  }
+  set {
+    name  = "mobius.persistentVolume.claimName"
+    value = var.mobius-kube["persistentVolume_claimName"]
+  }
+  set {
+    name  = "mobius.persistentVolume.size"
+    value = var.mobius-kube["persistentVolume_size"]
+  }
+
+  set {
+    name  = "mobius.mobiusDiagnostics.persistentVolume.volumeName"
+    value = var.mobius-kube["mobiusDiagnostics_persistentVolume_volumeName"]
+  }
+  set {
+    name  = "mobius.mobiusDiagnostics.persistentVolume.claimName"
+    value = var.mobius-kube["mobiusDiagnostics_persistentVolume_claimName"]
+  }
+  set {
+    name  = "mobius.mobiusDiagnostics.persistentVolume.size"
+    value = var.mobius-kube["mobiusDiagnostics_persistentVolume_size"]
+  }
+  set {
+    name  = "mobius.clustering.kubernetes.namespace"
     value = var.namespace
-   }    
+  }
 }
--- mobius/variables.tf
+++ mobius/variables.tf
@@ -9,86 +9,44 @@ variable "namespace" {
 }
 
 variable "mobius" {
-  type = list(object({
-    RDSPROVIDER=string
-    # secret key endpoint
-    RDSENDPOINT=string
-    RDSPORT= number
-    RDSPROTO= string
-    # secret key user
-    MOBIUSUSERNAME= string
-    # secret key schema
-    MOBIUSSCHEMANAME= string
-    # secret key password
-    MOBIUSSCHEMAPASSWORD= string
-    # secret key topicUrl
-    TOPIC_EXPORT_URL = string
-    # secret key topicUser
-    TOPIC_EXPORT_USER = string
-    # secret key topicPassword
-    TOPIC_EXPORT_PASSWORD = string
-    TOPIC_EXPORT_DRIVER=string    
-    DEFAULT_SSO_KEY= string
-    ENABLEINDEX= string
-    MOBIUS_FTS_ENGINE_TYPE= string
-    MOBIUS_FTS_SERVER_PROTOCOL= string
-    MOBIUS_FTS_HOST=string
-    MOBIUS_FTS_PORT= number
-    MOBIUS_FTS_INDEX_NAME= string
-    MOBIUS_ADMIN_USER= string
-    MOBIUS_ADMIN_GROUP= string
-  }))
-  default = [
-    {
-      RDSPROVIDER="POSTGRESQL"
-      RDSENDPOINT="postgres-postgresql.shared"
-      RDSPORT= 5432
-      RDSPROTO= "TCP"
-      MOBIUSUSERNAME= "mobiusserver12"
-      MOBIUSSCHEMANAME= "mobiusserver12"
-      MOBIUSSCHEMAPASSWORD= "postgres"
-      TOPIC_EXPORT_URL="jdbc:postgresql://${RDSENDPOINT}:${RDSPORT}/eventanalytics"
-      TOPIC_EXPORT_USER="eventanalytics"
-      TOPIC_EXPORT_PASSWORD="postgres"
-      TOPIC_EXPORT_DRIVER="org.postgresql.Driver"
-      DEFAULT_SSO_KEY= "ADASDFASDFXGGEG25585"
-      ENABLEINDEX= "YES"
-      MOBIUS_FTS_ENGINE_TYPE= "elasticsearch"
-      MOBIUS_FTS_SERVER_PROTOCOL= "HTTP"
-      MOBIUS_FTS_HOST= "elasticsearch-master.shared"
-      MOBIUS_FTS_PORT= 9200
-      MOBIUS_FTS_INDEX_NAME= "mobius12"
-      MOBIUS_ADMIN_USER= "admin"
-      MOBIUS_ADMIN_GROUP= "mobiusadmin"
+  type = map(string)
+  default = {
+      RDSPROVIDER                = "POSTGRESQL"
+      RDSENDPOINT                = "postgres-postgresql.shared"
+      RDSPORT                    = 5432
+      RDSPROTO                   = "TCP"
+      MOBIUSUSERNAME             = "mobiusserver12"
+      MOBIUSSCHEMANAME           = "mobiusserver12"
+      MOBIUSSCHEMAPASSWORD       = "postgres"
+      TOPIC_EXPORT_URL           = "jdbc:postgresql://postgres-postgresql.shared:5432/eventanalytics"
+      TOPIC_EXPORT_USER          = "eventanalytics"
+      TOPIC_EXPORT_PASSWORD      = "postgres"
+      TOPIC_EXPORT_DRIVER        = "org.postgresql.Driver"
+      DEFAULT_SSO_KEY            = "ADASDFASDFXGGEG25585"
+      ENABLEINDEX                = "YES"
+      MOBIUS_FTS_ENGINE_TYPE     = "elasticsearch"
+      MOBIUS_FTS_SERVER_PROTOCOL = "HTTP"
+      MOBIUS_FTS_HOST            = "elasticsearch-master.shared"
+      MOBIUS_FTS_PORT            = 9200
+      MOBIUS_FTS_INDEX_NAME      = "mobius12"
+      MOBIUS_ADMIN_USER          = "admin"
+      MOBIUS_ADMIN_GROUP         = "mobiusadmin"
     }
-  ]
+
 }
 
-
 variable "mobius-kube" {
-  type = list(object({
-    image_repository=string
-    image_tag=string
-    storageClassName= string
-    persistentVolume_volumeName= string
-    persistentVolume_claimName= string
-    persistentVolume_size= string
-    mobiusDiagnostics_persistentVolume_volumeName= string
-    mobiusDiagnostics_persistentVolume_claimName= string
-    mobiusDiagnostics_persistentVolume_size= string
-
-  }))
-  default = [
-    {
-    image_repository="registry.asg.com/mobius-server"
-    image_tag= "12.0.0"
-    storageClassName= "microk8s-hostpath"
-    persistentVolume_volumeName= "pv-mobius12-efs"
-    persistentVolume_claimName= "pvc-mobius12-efs"
-    persistentVolume_size= "1Gi"
-    mobiusDiagnostics_persistentVolume_volumeName= "pv-mobius12-diagnose"
-    mobiusDiagnostics_persistentVolume_claimName= "pvc-mobius12-diagnose"
-    mobiusDiagnostics_persistentVolume_size= "1Gi"
+  type = map(string)
+  default = {
+      image_repository                              = "registry.asg.com/mobius-server"
+      image_tag                                     = "12.0.0"
+      storageClassName                              = "microk8s-hostpath"
+      persistentVolume_volumeName                   = "pv-mobius12-efs"
+      persistentVolume_claimName                    = "pvc-mobius12-efs"
+      persistentVolume_size                         = "1Gi"
+      mobiusDiagnostics_persistentVolume_volumeName = "pv-mobius12-diagnose"
+      mobiusDiagnostics_persistentVolume_claimName  = "pvc-mobius12-diagnose"
+      mobiusDiagnostics_persistentVolume_size       = "1Gi"
     }
-  ]
+
 }
