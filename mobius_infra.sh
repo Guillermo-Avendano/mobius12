b9353cc4912f8457940fee768d278f9381fa9013
Guillermo Avendaño en linux <avendano.guillermo@gmail.com>
2023-04-10 17:56:16 -0300

    ultimo por hoy

--- mobius_infra.sh
+++ mobius_infra.sh
@@ -48,7 +48,10 @@ else
 
     elif [[ $option == "create" ]]; then
         echo "Creating mobius cluster"
-        k3d cluster create $CLUSTER -p "80:80@loadbalancer" -p "8900:30080@agent:0" -p "8901:30081@agent:0" -p "8902:30082@agent:0" --agents 2 --k3s-arg "--disable=traefik@server:0"
+
+        #k3d cluster create $CLUSTER -p "80:80@loadbalancer" -p "8900:30080@agent:0" -p "8901:30081@agent:0" -p "8902:30082@agent:0" --agents 2 --k3s-arg "--disable=traefik@server:0" --kube-apiserver-arg rate-limit-rps=100 
+        k3d cluster create $CLUSTER -p "80:80@loadbalancer" --agents 2 --k3s-arg "--disable=traefik@server:0" --kube-apiserver-arg rate-limit-rps=100
+
         k3d kubeconfig get $CLUSTER > ~/.kube/config
         kubectl config use-context k3d-$CLUSTER
 
