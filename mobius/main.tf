086a4f1ec56c97ebef3dc19aba875057a8203185
Guillermo Avendaño en linux <avendano.guillermo@gmail.com>
2023-04-10 15:50:10 -0300

    vA

--- mobius/main.tf
+++ mobius/main.tf
@@ -13,71 +13,35 @@ resource "kubernetes_namespace" "mobius" {
   }
 }
 
-resource "kubernetes_persistent_volume" "mobius12-efs" {
-  metadata {
-    name = "pv-mobius12-efs"
-  }
-  spec {
-    access_modes = ["ReadWriteMany"]
-    capacity = {
-      storage = "1Gi"
-    }
-    persistent_volume_source {
-      host_path {
-        path = "/home/rocket/pv-mobius12-efs"
-      }
-    }
-  }
-}
-
 resource "kubernetes_persistent_volume_claim" "mobius12-efs" {
   metadata {
     name = "pvc-mobius12-efs"
     namespace  = var.namespace
   }
   spec {
-    access_modes = ["ReadWriteMany"]
+    access_modes = ["ReadWriteOnce"]
     resources {
       requests = {
         storage = "1Gi"
       }
     }
-    volume_name = "${kubernetes_persistent_volume.mobius12-efs.metadata.0.name}"
   }
 }
 
-resource "kubernetes_persistent_volume" "mobius12-diag" {
-  metadata {
-    name = "pv-mobius12-diagnostics"
-  }
-  spec {
-    access_modes = ["ReadWriteMany"]
-    capacity = {
-      storage = "1Gi"
-    }
-    persistent_volume_source {
-      host_path {
-        path = "/home/rocket/pv-mobius12-diag"
-      }
-    }
-  }
-}
 resource "kubernetes_persistent_volume_claim" "mobius12-diag" {
   metadata {
     name = "pvc-mobius12-diagnose"
     namespace  = var.namespace
   }
   spec {
-    access_modes = ["ReadWriteMany"]
+    access_modes = ["ReadWriteOnce"]
     resources {
       requests = {
         storage = "1Gi"
       }
     }
-    volume_name = "${kubernetes_persistent_volume.mobius12-diag.metadata.0.name}"
   }
 }
-
 
 resource "kubernetes_secret" "mobius12" {
   metadata {
