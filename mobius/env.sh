8c6642a5e53989055f8322580e900e2de40fcc90
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-04-11 15:02:55 -0300

    update

--- mobius/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# env.sh
-
-# Change the contents of this output to get the environment variables
-# of interest. The output must be valid JSON, with strings for both
-# keys and values.
-export $(cat ../.env | egrep -v "(^#.*|^$)" | xargs)
-env
