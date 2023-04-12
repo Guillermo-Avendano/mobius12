cb96f9f6a813fa7203522c5da566c4b15ab5c074
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-04-12 16:51:01 -0300

    va

--- mobius/install.sh
+++ mobius/install.sh
@@ -5,8 +5,7 @@ source ../common/common.sh
 
 if ! [ -e $TF_VAR_MOBIUS_VIEW_URL.key ] && ! [ -e $TF_VAR_MOBIUS_VIEW_URL.crt]; then
   info_message "Generating certificates: $TF_VAR_MOBIUS_VIEW_URL.key and $TF_VAR_MOBIUS_VIEW_URL.crt"
-  openssl req -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -keyout "${$TF_VAR_MOBIUS_VIEW_URL}.key" -out "${$TF_VAR_MOBIUS_VIEW_URL}.crt" -subj "/CN=${$TF_VAR_MOBIUS_VIEW_URL}" -addext "subjectAltName=DNS:${$TF_VAR_MOBIUS_VIEW_URL}" -addext 'extendedKeyUsage=serverAuth,clientAuth'
-
+  openssl req -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -keyout "${TF_VAR_MOBIUS_VIEW_URL}.key" -out "${TF_VAR_MOBIUS_VIEW_URL}.crt" -subj "/CN=${TF_VAR_MOBIUS_VIEW_URL}" -addext "subjectAltName=DNS:${TF_VAR_MOBIUS_VIEW_URL}" -addext 'extendedKeyUsage=serverAuth,clientAuth'
 fi
 
 kubectl create namespace $TF_VAR_NAMESPACE
