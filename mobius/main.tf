7bbb0ccd18e0af7813e5c67ab231162e94f157bf
Guillermo Avendano <gavendano@rocketsoftware.com>
2023-04-12 17:17:29 -0300

    va

--- mobius/main.tf
+++ mobius/main.tf
@@ -326,12 +326,14 @@ resource "helm_release" "mobiusview12" {
             ]
           }
         ]
+      /*
         tls = [
         {
           secretName = var.MOBIUS_VIEW_TLS_SECRET
           hosts = [ var.MOBIUS_VIEW_URL ]
         }
       ]
+      */
       }
     })
   ]
